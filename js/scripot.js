05baa590bf2274d82c07376ee24266d05a2443af
Azat Shaqirov <azat.shaqirov@gmail.com>
2024-10-11 12:19:39 +0300

    first cycles(movie)

--- js/scripot.js
+++ js/scripot.js
@@ -1,21 +1,37 @@
-"use strict";
+"use strict"
 
-// console.log('arr' + "-object")
-// console.log(4 + +"4")
+const numberOfFilms = +prompt('How many movies did you watched?', '');
 
-// let incr = 10,
-//     decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: {},
+    privat: false
+};
 
-// // ++incr;
-// // --decr;  
 
-// console.log(++incr)
-// console.log(--decr)
+for (let i = 0; i < 2; i++) {
+    const a = prompt ('One of last watched movie?', ''),
+          b = prompt ('What rating can you give to this film?');
 
-// console.log(5%2)
+          if (a != '' &&  b != '' && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            alert('done!')
+          } else {
+            alert('Wrong input!')
+            i--;
+          }
+}
 
-// console.log(2*4 === 8);
+if (personalMovieDB.count < 10) {
+    alert('Quite a few movies have been watched.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('You are classic viewer.');
+} else if (personalMovieDB.count > 30) {
+    alert("You're a movie fan.") ;
+} else {
+    alert("An error has occurred.")
+}
 
-const isChecked = false,
-      isClose = false;
-console.log(2 + 2 * 2 !== 6);
+console.log(personalMovieDB);
